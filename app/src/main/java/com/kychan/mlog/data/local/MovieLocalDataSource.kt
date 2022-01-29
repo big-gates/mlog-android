7ed4d74317ab451e5e6ace9eda6b1eb2ec0d4184
ko0chan <kychan1320@gmail.com>
2022-01-29 17:35:37 +0900

    refactor : local data source 추상화 및 di 적용

--- app/src/main/java/com/kychan/mlog/data/local/MovieLocalDataSource.kt
+++ app/src/main/java/com/kychan/mlog/data/local/MovieLocalDataSource.kt
@@ -5,22 +5,32 @@ import com.kychan.mlog.data.local.dao.MovieDao
 import com.kychan.mlog.data.local.model.MovieEntity
 import javax.inject.Inject
 
-class MovieLocalDataSource @Inject constructor(
+interface MovieLocalDataSource {
+    fun getMovieAll(): DataSource.Factory<Int, MovieEntity>
+    
+    fun insertMovie(movieEntity: MovieEntity)
+    
+    fun deleteMovie(link: String)
+    
+    fun updateMovie(evaluation: Float, link: String)
+}
+
+class MovieLocalDataSourceImpl @Inject constructor(
     private val movieDao: MovieDao
-) {
-    fun getMovieAll(): DataSource.Factory<Int, MovieEntity> {
+) : MovieLocalDataSource {
+    override fun getMovieAll(): DataSource.Factory<Int, MovieEntity> {
         return movieDao.getMovieAll()
     }
-
-    fun insertMovie(movieEntity: MovieEntity) {
+    
+    override fun insertMovie(movieEntity: MovieEntity) {
         movieDao.insert(movieEntity)
     }
-
-    fun deleteMovie(link: String) {
+    
+    override fun deleteMovie(link: String) {
         movieDao.delete(link)
     }
-
-    fun updateMovie(evaluation: Float, link: String){
+    
+    override fun updateMovie(evaluation: Float, link: String) {
         movieDao.update(evaluation, link)
     }
 }
