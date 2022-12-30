81aa97cfedb7d3820e46edee6b0e37c237e9bd0d
hare <khm0651@gmail.com>
2022-12-30 19:09:35 +0900

    feat: add discover movie endpoint

--- core/dataSourceRemote/http/src/main/java/com/kychan/mlog/core/dataSourceRemote/http/api/RetrofitTMDBApi.kt
+++ core/dataSourceRemote/http/src/main/java/com/kychan/mlog/core/dataSourceRemote/http/api/RetrofitTMDBApi.kt
@@ -1,18 +1,24 @@
 package com.kychan.mlog.core.dataSourceRemote.http.api
 
+import com.kychan.mlog.core.dataSourceRemote.http.model.MovieDiscoverRes
 import com.kychan.mlog.core.dataSourceRemote.http.model.TvSeriesDetailRes
 import com.kychan.mlog.core.dataSourceRemote.http.model.TvSeriesRes
+import com.kychan.mlog.core.model.Language
+import com.kychan.mlog.core.model.WatchProviders
+import com.kychan.mlog.core.model.WatchRegion
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RetrofitTMDBApi {
 
-    //    @GET("/movie/popular")
-//    suspend fun getMoviePopular(
-//        @Query("page") page: Int,
-//        @Query("language") language: String
-//    ):
+    @GET("/3/discover/movie")
+    suspend fun getMoviePopularWithProvider(
+        @Query("page") page: Int,
+        @Query("language") language: Language,
+        @Query("watch_region") watchRegion: WatchRegion,
+        @Query("with_watch_providers") withWatchProviders: WatchProviders
+    ): MovieDiscoverRes
 
     @GET("/3/tv/popular")
     suspend fun getTvSeriesPopular(
