ee47937201c1df10452d9201857ecab3f2079eca
ko0chan <kychan1320@gmail.com>
2023-02-23 18:24:35 +0900

    feat : 탭별 보관목록 뷰 임시 구현

--- feature/mypage/src/main/java/com/kychan/mlog/feature/mypage/navigation/MyPageNavigation.kt
+++ feature/mypage/src/main/java/com/kychan/mlog/feature/mypage/navigation/MyPageNavigation.kt
@@ -4,6 +4,7 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import com.kychan.mlog.feature.mypage.MyPageScreen
 
 const val myPageRoute = "myPage_route"
 
