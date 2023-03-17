a1f0c553b32ac58b25b25d434b496e1b10af9563
hare <khm0651@gmail.com>
2023-03-17 19:50:58 +0900

    chore: search animation

--- feature/search/src/main/java/com/kychan/mlog/feature/search/navigation/SearchNavigation.kt
+++ feature/search/src/main/java/com/kychan/mlog/feature/search/navigation/SearchNavigation.kt
@@ -2,6 +2,7 @@ package com.kychan.mlog.feature.search.navigation
 
 import androidx.compose.animation.*
 import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
@@ -9,6 +10,7 @@ import com.google.accompanist.navigation.animation.composable
 import com.kychan.mlog.feature.search.SearchRouter
 
 const val searchRoute = "search_route"
+private val slideInitialOffsetPadding = 30.dp.value
 
 fun NavController.navigateToSearch(navOptions: NavOptions? = null) {
     this.navigate(searchRoute, navOptions)
@@ -19,10 +21,10 @@ fun NavGraphBuilder.searchScreen(){
     composable(
         route = searchRoute,
         enterTransition = { slideIn(
-            initialOffset = { IntOffset(it.width,0) }
+            initialOffset = { IntOffset((it.width - slideInitialOffsetPadding).toInt(),0) }
         ) },
         popEnterTransition = { slideIn(
-            initialOffset = { IntOffset(it.width,0) }
+            initialOffset = { IntOffset((it.width - slideInitialOffsetPadding).toInt(),0) }
         ) },
     ){
         SearchRouter()
