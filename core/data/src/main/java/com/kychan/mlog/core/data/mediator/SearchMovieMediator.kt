1bf5f516a16453d46c499f195e66021861b69720
hare <khm0651@gmail.com>
2023-04-03 15:13:02 +0200

    feat: add getSearch in repository

---
+++ core/data/src/main/java/com/kychan/mlog/core/data/mediator/SearchMovieMediator.kt
@@ -0,0 +1,47 @@
+package com.kychan.mlog.core.data.mediator
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.kychan.mlog.core.data.mapper.toModel
+import com.kychan.mlog.core.dataSourceRemote.http.datasource.TMDBDataSource
+import com.kychan.mlog.core.model.Language
+import com.kychan.mlog.core.model.MediaType
+import com.kychan.mlog.core.model.Movie
+import com.kychan.mlog.core.model.WatchRegion
+
+class SearchMovieMediator(
+    private val tmdbDataSource: TMDBDataSource,
+    private val query: String,
+    private val language: Language,
+    private val watchRegion: WatchRegion
+): PagingSource<Int, Movie>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Movie> {
+        return try {
+            val nextPageNumber = params.key ?: 1
+            val response = tmdbDataSource.getSearch(
+                page = nextPageNumber,
+                language = language,
+                watchRegion = watchRegion,
+                query = query
+            )
+            LoadResult.Page(
+                data = response.toModel(
+                    page = nextPageNumber,
+                    filters = listOf(MediaType.MOVIE, MediaType.TV)
+                ),
+                prevKey = null,
+                nextKey = if(response.results.isEmpty()) null else response.page + 1
+            )
+        } catch(e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Movie>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+}
--- core/data/src/main/java/com/kychan/mlog/core/data/repository/SearchRepositoryImpl.kt
+++ core/data/src/main/java/com/kychan/mlog/core/data/repository/SearchRepositoryImpl.kt
@@ -1,11 +1,18 @@
 package com.kychan.mlog.core.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.kychan.mlog.core.data.mapper.toRecentSearchEntity
+import com.kychan.mlog.core.data.mediator.SearchMovieMediator
 import com.kychan.mlog.core.dataSourceLocal.room.datasource.RoomDataSource
 import com.kychan.mlog.core.dataSourceLocal.room.model.RecentSearchEntity
 import com.kychan.mlog.core.dataSourceLocal.room.model.toDomain
 import com.kychan.mlog.core.dataSourceRemote.http.datasource.TMDBDataSource
+import com.kychan.mlog.core.model.Language
+import com.kychan.mlog.core.model.Movie
 import com.kychan.mlog.core.model.RecentSearch
+import com.kychan.mlog.core.model.WatchRegion
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -14,6 +21,22 @@ class SearchRepositoryImpl @Inject constructor(
     private val tmdbDataSource: TMDBDataSource,
     private val roomDataSource: RoomDataSource
 ): SearchRepository {
+    override fun getSearch(
+        language: Language,
+        watchRegion: WatchRegion,
+        query: String
+    ): Flow<PagingData<Movie>> {
+        return Pager(
+            PagingConfig(pageSize = 20)
+        ) {
+            SearchMovieMediator(
+                tmdbDataSource,
+                query,
+                language,
+                watchRegion
+            )
+        }.flow
+    }
 
     override suspend fun updateRecentSearch(text: String) {
         roomDataSource.updateRecentSearch(text.toRecentSearchEntity())
