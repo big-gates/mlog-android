95d2a8ce163072ed9dd1345c86fe721d25f8bbc5
hare <khm0651@gmail.com>
2023-04-29 17:40:15 +0900

    refactor: add update if exist entity else insert

--- core/dataSourceLocal/room/src/main/java/com/kychan/mlog/core/dataSourceLocal/room/model/RecentSearchEntity.kt
+++ core/dataSourceLocal/room/src/main/java/com/kychan/mlog/core/dataSourceLocal/room/model/RecentSearchEntity.kt
@@ -4,6 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import com.kychan.mlog.core.common.extenstions.toDateTimeFormat
 import com.kychan.mlog.core.model.RecentSearch
 
 @Entity(
@@ -20,4 +21,14 @@ data class RecentSearchEntity (
 fun RecentSearchEntity.toDomain(): RecentSearch = RecentSearch(
     id = id?: throw IllegalArgumentException("RecentSearchEntity ID is NULL this is WHAT THE HELL"),
     text = text
+)
+
+fun RecentSearchEntity.update(
+    id: Int? = null,
+    text: String? = null,
+    createdAt: String = System.currentTimeMillis().toDateTimeFormat()
+) = copy(
+    id = id?: this.id,
+    text = text?: this.text,
+    createdAt = createdAt
 )
