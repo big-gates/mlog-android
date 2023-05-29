f2a62a1126acf24f48b96ef37edf6b0c517c7763
ko0chan <kychan1320@gmail.com>
2023-05-29 18:00:35 +0900

    fix : 영화 상세 페이지 좋아요 상태 직렬화

--- core/dataSourceLocal/room/src/main/java/com/kychan/mlog/core/dataSourceLocal/room/model/MyMovieRatedAndWantedVO.kt
+++ core/dataSourceLocal/room/src/main/java/com/kychan/mlog/core/dataSourceLocal/room/model/MyMovieRatedAndWantedVO.kt
@@ -1,15 +1,16 @@
 package com.kychan.mlog.core.dataSourceLocal.room.model
 
+import androidx.room.ColumnInfo
 import com.kychan.mlog.core.model.MyMovieRatedAndWanted
 
 data class MyMovieRatedAndWantedVO(
     val rated: Float?,
     val comment: String?,
-    val wantToMovieId: Int?,
+    @ColumnInfo("my_movie_id") val wantToMovieId: Int?,
 )
 
-fun MyMovieRatedAndWantedVO?.toDomain() = MyMovieRatedAndWanted(
-    rated = this?.rated ?: 0f,
-    comment = this?.comment.orEmpty(),
-    isLike = (this?.wantToMovieId ?: -1) > -1,
+fun MyMovieRatedAndWantedVO.toDomain() = MyMovieRatedAndWanted(
+    rated = rated ?: 0f,
+    comment = comment.orEmpty(),
+    isLike = (wantToMovieId ?: -1) > -1,
 )
